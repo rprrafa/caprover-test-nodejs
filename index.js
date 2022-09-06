4c33022cdfdf2032c5593caff5c10a35f1c6c3bd
Rafael Ribeiro <rpr.rafa@gmail.com>
2022-09-06 13:43:11 -0300

    testing webhook

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`Servidor rodando em http://localhost:${PORT}`) });
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello CapRover</h1>');
+    response.send('<h1>Hello CapRover!</h1>');
 });
